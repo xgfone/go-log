a44584c6f99b5be2df970475b50bc931a82a0e9f
xgfone <xgfone@126.com>
2021-11-16 20:56:36 +0800

    fix: rename DefalutLogger to DefaultLogger

--- global.go
+++ global.go
@@ -14,130 +14,130 @@
 
 package log
 
-// DefalutLogger is the default global logger.
-var DefalutLogger = New("root")
+// DefaultLogger is the default global logger.
+var DefaultLogger = New("root")
 
-// GetLevel is equal to DefalutLogger.GetLevel() to return the level
+// GetLevel is equal to DefaultLogger.GetLevel() to return the level
 // of the default logger.
-func GetLevel() Level { return DefalutLogger.GetLevel() }
+func GetLevel() Level { return DefaultLogger.GetLevel() }
 
-// SetLevel is equal to DefalutLogger.SetLevel(level) to reset the level
+// SetLevel is equal to DefaultLogger.SetLevel(level) to reset the level
 // of the default logger.
-func SetLevel(level Level) { DefalutLogger.SetLevel(level) }
+func SetLevel(level Level) { DefaultLogger.SetLevel(level) }
 
-// WithCtx is equal to DefalutLogger.WithCtx(fields...).
-func WithCtx(fields ...Field) *Logger { return DefalutLogger.WithCtx(fields...) }
+// WithCtx is equal to DefaultLogger.WithCtx(fields...).
+func WithCtx(fields ...Field) *Logger { return DefaultLogger.WithCtx(fields...) }
 
-// WithName is equal to DefalutLogger.WithName(name).
-func WithName(name string) *Logger { return DefalutLogger.WithName(name) }
+// WithName is equal to DefaultLogger.WithName(name).
+func WithName(name string) *Logger { return DefaultLogger.WithName(name) }
 
-// WithLevel is equal to DefalutLogger.WithLevel(level).
-func WithLevel(level Level) *Logger { return DefalutLogger.WithLevel(level) }
+// WithLevel is equal to DefaultLogger.WithLevel(level).
+func WithLevel(level Level) *Logger { return DefaultLogger.WithLevel(level) }
 
-// WithEncoder is equal to DefalutLogger.WithEncoder(enc).
-func WithEncoder(enc Encoder) *Logger { return DefalutLogger.WithEncoder(enc) }
+// WithEncoder is equal to DefaultLogger.WithEncoder(enc).
+func WithEncoder(enc Encoder) *Logger { return DefaultLogger.WithEncoder(enc) }
 
-// WithDepth is equal to DefalutLogger.WithDepth(depth).
-func WithDepth(depth int) *Logger { return DefalutLogger.WithDepth(depth) }
+// WithDepth is equal to DefaultLogger.WithDepth(depth).
+func WithDepth(depth int) *Logger { return DefaultLogger.WithDepth(depth) }
 
-// Trace is equal to DefalutLogger.Trace(msg, fields...).
+// Trace is equal to DefaultLogger.Trace(msg, fields...).
 func Trace(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlTrace, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlTrace, 1, msg, nil, fields)
 }
 
-// Debug is equal to DefalutLogger.Debug(msg, fields...).
+// Debug is equal to DefaultLogger.Debug(msg, fields...).
 func Debug(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlDebug, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlDebug, 1, msg, nil, fields)
 }
 
-// Info is equal to DefalutLogger.Info(msg, fields...).
+// Info is equal to DefaultLogger.Info(msg, fields...).
 func Info(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlInfo, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlInfo, 1, msg, nil, fields)
 }
 
-// Warn is equal to DefalutLogger.Warn(msg, fields...).
+// Warn is equal to DefaultLogger.Warn(msg, fields...).
 func Warn(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlWarn, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlWarn, 1, msg, nil, fields)
 }
 
-// Error is equal to DefalutLogger.Error(msg, fields...).
+// Error is equal to DefaultLogger.Error(msg, fields...).
 func Error(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlError, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlError, 1, msg, nil, fields)
 }
 
-// Fatal is equal to DefalutLogger.Fatal(msg, fields...).
+// Fatal is equal to DefaultLogger.Fatal(msg, fields...).
 func Fatal(msg string, fields ...Field) {
-	DefalutLogger.Log(LvlFatal, 1, msg, nil, fields)
+	DefaultLogger.Log(LvlFatal, 1, msg, nil, fields)
 }
 
-// Traces is equal to DefalutLogger.Traces.
+// Traces is equal to DefaultLogger.Traces.
 func Traces(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlTrace, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlTrace, 1, msg, keyAndValues)
 }
 
-// Debugs is equal to DefalutLogger.Debugs.
+// Debugs is equal to DefaultLogger.Debugs.
 func Debugs(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlDebug, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlDebug, 1, msg, keyAndValues)
 }
 
-// Infos is equal to DefalutLogger.Infos.
+// Infos is equal to DefaultLogger.Infos.
 func Infos(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlInfo, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlInfo, 1, msg, keyAndValues)
 }
 
-// Warns is equal to DefalutLogger.Warns.
+// Warns is equal to DefaultLogger.Warns.
 func Warns(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlWarn, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlWarn, 1, msg, keyAndValues)
 }
 
-// Errors equal to DefalutLogger.Errors.
+// Errors equal to DefaultLogger.Errors.
 func Errors(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlError, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlError, 1, msg, keyAndValues)
 }
 
-// Fatals is equal to DefalutLogger.Fatals.
+// Fatals is equal to DefaultLogger.Fatals.
 func Fatals(msg string, keyAndValues ...interface{}) {
-	DefalutLogger.logs(LvlFatal, 1, msg, keyAndValues)
+	DefaultLogger.logs(LvlFatal, 1, msg, keyAndValues)
 }
 
-// Tracef is equal to DefalutLogger.Tracef(msg, args...).
+// Tracef is equal to DefaultLogger.Tracef(msg, args...).
 func Tracef(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlTrace, 1, msg, args, nil)
+	DefaultLogger.Log(LvlTrace, 1, msg, args, nil)
 }
 
-// Debugf is equal to DefalutLogger.Debugf(msg, args...).
+// Debugf is equal to DefaultLogger.Debugf(msg, args...).
 func Debugf(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlDebug, 1, msg, args, nil)
+	DefaultLogger.Log(LvlDebug, 1, msg, args, nil)
 }
 
-// Infof is equal to DefalutLogger.Infof(msg, args...).
+// Infof is equal to DefaultLogger.Infof(msg, args...).
 func Infof(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlInfo, 1, msg, args, nil)
+	DefaultLogger.Log(LvlInfo, 1, msg, args, nil)
 }
 
-// Warnf is equal to DefalutLogger.Warnf(msg, args...).
+// Warnf is equal to DefaultLogger.Warnf(msg, args...).
 func Warnf(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlWarn, 1, msg, args, nil)
+	DefaultLogger.Log(LvlWarn, 1, msg, args, nil)
 }
 
-// Errorf is equal to DefalutLogger.Errorf(msg, args...).
+// Errorf is equal to DefaultLogger.Errorf(msg, args...).
 func Errorf(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlError, 1, msg, args, nil)
+	DefaultLogger.Log(LvlError, 1, msg, args, nil)
 }
 
-// Fatalf is equal to DefalutLogger.Fatalf(msg, args...).
+// Fatalf is equal to DefaultLogger.Fatalf(msg, args...).
 func Fatalf(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlFatal, 1, msg, args, nil)
+	DefaultLogger.Log(LvlFatal, 1, msg, args, nil)
 }
 
-// Printf is equal to DefalutLogger.Infof(msg, args...).
+// Printf is equal to DefaultLogger.Infof(msg, args...).
 func Printf(msg string, args ...interface{}) {
-	DefalutLogger.Log(LvlInfo, 1, msg, args, nil)
+	DefaultLogger.Log(LvlInfo, 1, msg, args, nil)
 }
 
-// Ef is equal to DefalutLogger.Error(fmt.Sprintf(msg, args), E(err)).
+// Ef is equal to DefaultLogger.Error(fmt.Sprintf(msg, args), E(err)).
 func Ef(err error, format string, args ...interface{}) {
-	DefalutLogger.Log(LvlError, 1, format, args, []Field{E(err)})
+	DefaultLogger.Log(LvlError, 1, format, args, []Field{E(err)})
 }
 
 // IfErr logs the message and fields with the ERROR level
@@ -150,6 +150,6 @@ func IfErr(err error, msg string, fields ...Field) {
 			fields = append(fields, E(err))
 		}
 
-		DefalutLogger.Log(LvlError, 1, msg, nil, fields)
+		DefaultLogger.Log(LvlError, 1, msg, nil, fields)
 	}
 }
