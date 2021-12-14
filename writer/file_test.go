f6fe7f0bd48f23c4af339b7c4a83493aac882c89
xgfone <xgfone@126.com>
2021-12-15 00:16:18 +0800

    fix a test error

--- writer/file_test.go
+++ writer/file_test.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !go.1.16
-// +build !go.1.16
+//go:build go1.16
+// +build go1.16
 
 package writer
 
